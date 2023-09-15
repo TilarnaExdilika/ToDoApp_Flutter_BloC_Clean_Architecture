a06966e50799b0c21eee2f2e5e7b6d77eb84ce31
Sang Phạm <youlovelong123@gmail.com>
2023-09-15 16:30:22 +0700

    LockScreen UI
    --Flutter

--- lib/ui/lock_screen.dart
+++ lib/ui/lock_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:tilarna_todo_app/ui/todo_screen.dart';
+import 'package:tilarna_todo_app/ui/widgets/buttons.dart';
+import 'package:tilarna_todo_app/ui/widgets/custom_app_bar.dart';
 import 'package:tilarna_todo_app/util/constants.dart';
 import 'package:tilarna_todo_app/util/navigator.dart';
 
@@ -18,98 +21,94 @@ class _LockScreenState extends State<LockScreen> {
     return Scaffold(
         body: Column(
       children: [
-        SizedBox(
-          height: kPad(context) * .1,
-        ),
-        Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: kPad(context) * 0.25),
-            child: Text(
-              _code.toString(),
-              style: style(context).copyWith(
-                fontSize: kPad(context) * .1,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: kPad(context) * 0.05),
-          child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 9,
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3, childAspectRatio: 1.3),
-              itemBuilder: (context, index) {
-                return numberButton(
-                    context,
-                    Text(
-                      '${index + 1}',
-                      style: style(context).copyWith(
-                        fontSize: kPad(context) * 0.05,
-                      ),
-                    ), () {
-                  setState(() {
-                    _code = (_code * 10) + (index + 1);
-                  });
-                });
-              }),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: kPad(context) * .08),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        customAppBar(context, 'Enter the code', Iconsax.home3, Iconsax.home3,
+            () {}, () {}),
+        Expanded(
+          child: Column(
             children: [
-              numberButton(
-                  context,
-                  Icon(
-                    CupertinoIcons.delete_left,
-                    size: kPad(context) * .05,
-                    color: Colors.red,
-                  ), () {
-                setState(() {
-                  _code = (_code ~/ 10);
-                });
-              }),
-              numberButton(
-                  context,
-                  Text(
-                    '0',
-                    style:
-                        style(context).copyWith(fontSize: kPad(context) * 0.05),
-                  ), () {
-                setState(() {
-                  _code = (_code * 10) + 0;
-                });
-              }),
-              numberButton(
-                  context,
-                  Icon(
-                    CupertinoIcons.check_mark,
-                    size: kPad(context) * .05,
-                    color: Colors.green,
-                  ), () {
-                push(context, const TodoScreen());
-              }),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: kPad(context) * 0.1),
+                  child: Text(
+                    _code.toString(),
+                    style: style(context).copyWith(
+                      fontSize: kPad(context) * .1,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kPad(context) * 0.05),
+                child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: 9,
+                    shrinkWrap: true,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3, childAspectRatio: 1.3),
+                    itemBuilder: (context, index) {
+                      return numberButton(
+                          context,
+                          Text(
+                            '${index + 1}',
+                            style: style(context).copyWith(
+                              fontSize: kPad(context) * 0.05,
+                            ),
+                          ), () {
+                        setState(() {
+                          _code = (_code * 10) + (index + 1);
+                        });
+                      });
+                    }),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kPad(context) * .08),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    numberButton(
+                        context,
+                        Icon(
+                          CupertinoIcons.delete_left,
+                          size: kPad(context) * .05,
+                          color: Colors.red,
+                        ), () {
+                      setState(() {
+                        _code = (_code ~/ 10);
+                      });
+                    }),
+                    numberButton(
+                        context,
+                        Text(
+                          '0',
+                          style: style(context)
+                              .copyWith(fontSize: kPad(context) * 0.05),
+                        ), () {
+                      setState(() {
+                        _code = (_code * 10) + 0;
+                      });
+                    }),
+                    numberButton(
+                        context,
+                        Icon(
+                          CupertinoIcons.check_mark,
+                          size: kPad(context) * .05,
+                          color: Colors.green,
+                        ), () {
+                      push(context, const TodoScreen());
+                    }),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: kPad(context) * .1,
+              ),
+              blueButton(context, 'Register Instead', () {}),
             ],
           ),
-        )
+        ),
       ],
     ));
-  }
-
-  IconButton numberButton(
-      BuildContext context, Widget icon, void Function() onTap) {
-    return IconButton(
-        onPressed: onTap,
-        iconSize: kPad(context) * .2,
-        icon: Container(
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: dark.withOpacity(0.1))),
-          padding: EdgeInsets.all(kPad(context) * .06),
-          child: icon,
-        ));
   }
 }
---
+++ lib/ui/widgets/custom_app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:tilarna_todo_app/util/constants.dart';
+
+Widget customAppBar(
+    BuildContext context,
+    String title,
+    IconData firstIcon,
+    IconData secondIcon,
+    void Function() firstIconFunction,
+    void Function() secondIconFuntion) {
+  return Container(
+    //
+    height: kPad(context) * .25,
+    width: kPad(context),
+    decoration: BoxDecoration(),
+    child: Center(
+      child: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+              onPressed: firstIconFunction,
+              icon: Icon(
+                firstIcon,
+                color: dark.withOpacity(.5),
+              )),
+          Text(
+            title,
+            style: style(context).copyWith(
+              fontSize: kPad(context) * .04,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          IconButton(
+              onPressed: secondIconFuntion,
+              icon: Icon(
+                secondIcon,
+                color: dark.withOpacity(.5),
+              )),
+        ],
+      )),
+    ),
+  );
+}
